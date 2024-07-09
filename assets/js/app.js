46f7c5c9d95cf14d6430f747b56f4edbd395e229
tusharswami534 <tusharswami17@gmail.com>
2024-07-09 10:24:18 +0530

    first commit

---
+++ assets/js/app.js
@@ -0,0 +1,7 @@
+const navbarlinks = () => {
+    document.getElementById("nav-btn").classList.toggle("show-nav")
+    document.getElementById("ROT-1").classList.toggle("rotated")
+    document.getElementById("ROT-2").classList.toggle("rotated-2")
+    document.getElementById("d-n").classList.toggle("bg-transparent")
+    
+}
