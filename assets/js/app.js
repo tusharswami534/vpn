84209faf46bbbac6871f770116740a4a8e387b46
tusharswami534 <tusharswami17@gmail.com>
2024-07-12 13:50:56 +0530

    plan section complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,17 @@
 const navbarlinks = () => {
     document.getElementById("nav-btn").classList.toggle("show-nav")
+    document.body.classList.toggle("overflow-hidden")
     document.getElementById("ROT-1").classList.toggle("rotated")
     document.getElementById("ROT-2").classList.toggle("rotated-2")
     document.getElementById("d-n").classList.toggle("bg-transparent")
-    
+
+}
+document.querySelector("#basicToastBtn").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast')).show();
+}
+document.querySelector("#basicToastBtn-2").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast-2')).show();
+}
+document.querySelector("#basicToastBtn-3").onclick = function () {
+    new bootstrap.Toast(document.querySelector('#basicToast-3')).show();
 }
