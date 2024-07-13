a8487d81dd58e9994dcef9c406bb89d1bd86d21b
tusharswami534 <tusharswami17@gmail.com>
2024-07-13 10:25:26 +0530

    footer complete

--- assets/js/app.js
+++ assets/js/app.js
@@ -15,3 +15,65 @@ document.querySelector("#basicToastBtn-2").onclick = function () {
 document.querySelector("#basicToastBtn-3").onclick = function () {
     new bootstrap.Toast(document.querySelector('#basicToast-3')).show();
 }
+
+const preloader = document.getElementById("loder");
+function loding() {
+    preloader.style.display = "none";
+}
+const navbtn = () => {
+    document.getElementById("nav-menu").classList.toggle("show-nav")
+    document.body.classList.toggle("overflow-hidden")
+}
+
+
+$(".sliderbox").slick({
+    dots: true,
+    infinite: true,
+    speed: 300,
+    nextArrow: ".next",
+    prevArrow: ".prev",
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    autoplay: false,
+    autoplaySpeed: 2000,
+    infinite: true,
+    variableWidth: true,
+    responsive: [
+        {
+            breakpoint: 1024,
+            settings: {
+                slidesToShow: 3,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: true,
+
+            }
+        },
+        {
+            breakpoint: 992,
+            settings: {
+                slidesToShow: 2,
+                slidesToScroll: 1,
+                infinite: true,
+                dots: false,
+            }
+        },
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1
+            }
+        },
+        {
+            breakpoint: 576,
+            settings: {
+                slidesToShow: 1,
+                slidesToScroll: 1,
+                autoplay: false,
+                autoplaySpeed: 2000,
+                dots: true,
+            }
+        }
+    ]
+});
